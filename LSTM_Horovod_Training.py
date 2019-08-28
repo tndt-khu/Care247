9ed418314f61414caddc435ea981a59a4e633271
tndt-khu <53044052+tndt-khu@users.noreply.github.com>
2019-08-28 20:17:54 +0900

    Update

--- LSTM_Horovod_Training.py
+++ LSTM_Horovod_Training.py
@@ -104,8 +104,8 @@ def main(_):
     hvd.init()
 
     # delete previous saving checkpoints and model
-    if os.path.exists("./checkpoints") and os.path.isdir("./checkpoints"):
-        shutil.rmtree("./checkpoints")
+    if os.path.exists('./checkpoints') and os.path.isdir('./checkpoints'):
+        shutil.rmtree('./checkpoints')
     if os.path.exists(os.path.join(home, 'model')) and os.path.isdir(os.path.join(home, 'model')):
         shutil.rmtree(os.path.join(home, 'model'))
 
@@ -169,7 +169,7 @@ def main(_):
         hvd.BroadcastGlobalVariablesHook(0),
 
         # Horovod: adjust number of steps based on number of GPUs.
-        tf.train.StopAtStepHook(last_step=200 // hvd.size()),
+        tf.train.StopAtStepHook(last_step=4000 // hvd.size()),
 
         tf.train.LoggingTensorHook(tensors={'step': global_step, 'loss': loss},
                                    every_n_iter=10),
